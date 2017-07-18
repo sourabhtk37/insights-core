2e6385bc9a6c25a27bcf8f6ba2fc5e6ccf34fe8d
Kyle Lape <klape@redhat.com>
2017-07-18 11:01:08 -0500

    Remove stderr handler
    Since the stdout handler already prints ERROR messages, the stderr
    handler just duplicates all those messages to the console.

--- insights/client/client.py
+++ insights/client/client.py
@@ -73,9 +73,6 @@ def set_up_logging():
 
         # Send anything INFO+ to stdout and log
         stdout_handler = logging.StreamHandler(sys.stdout)
-        stderr_handler = logging.StreamHandler(sys.stderr)
-        stderr_handler.setLevel(logging.ERROR)
-        logging.root.addHandler(stderr_handler)
         if not opts.verbose:
             stdout_handler.setLevel(logging.INFO)
         if opts.quiet:
