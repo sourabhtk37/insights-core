528e7faba67d5d7c128df7bda5bf44fe5e650bb7
Bob Fahr <bfahr@redhat.com>
2017-09-29 16:22:16 -0500

    Ignore jupyter temp files

--- docs/conf.py
+++ docs/conf.py
@@ -78,7 +78,7 @@ release = insights.package_info["VERSION"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
